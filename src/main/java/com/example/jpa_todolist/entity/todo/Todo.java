f357608f5fc34967a58e01fd5293516a08f7b8dd
alsqja <alsqha2626@gmail.com>
2024-11-13 20:13:55 +0900

    feat: entity 추가
    - BaesEntity 로 JPA Auditing

---
+++ src/main/java/com/example/jpa_todolist/entity/todo/Todo.java
@@ -0,0 +1,46 @@
+package com.example.jpa_todolist.entity.todo;
+
+import com.example.jpa_todolist.entity.BaseEntity;
+import com.example.jpa_todolist.entity.user.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "todo")
+@Getter
+public class Todo extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String contents;
+
+    @Setter
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Todo(String title, String contents) {
+        this.title = title;
+        this.contents = contents;
+    }
+
+    public Todo() {
+
+    }
+}
---
+++ src/main/java/com/example/jpa_todolist/entity/user/User.java
@@ -0,0 +1,26 @@
+package com.example.jpa_todolist.entity.user;
+
+import com.example.jpa_todolist.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "user")
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}
